8f14cfd0b48a79076b725b11c0e5f2b5b8be4e45
Okuhle_Mhambi <bukhosimhambi@gmail.com>
2021-04-14 01:48:27 +0200

    alll

---
+++ greet-factory.js
@@ -0,0 +1,23 @@
+function greetFactoryFunction(){
+
+    var theName = "";
+    var theNameObject = {};
+    var theCounter = 0;
+
+    function setName(){
+
+    }
+    function setCounter(){
+
+    }
+    function getName(){
+
+    }
+    function getTotalNumber(){
+        
+    }
+    return {
+        
+    }
+
+}
---
+++ greet.js
@@ -0,0 +1,84 @@
+var userInput = document.querySelector(".enterName");
+var greetUser = document.querySelector(".greet");
+var returnUser = document.querySelector(".setOutput");
+
+var greetUserBtn = document.querySelector(".getName");
+var clearUserBtn = document.querySelector(".clearCount");
+
+var countUsers = document.querySelector(".counter");
+var counter = 0;
+var names = {};
+
+var greetInIsixhosa = "Mholo, ";
+var greetInSesotho = "Dumelang, ";
+var greetInXitsonga = "Ahee, ";
+
+function greetFunction() {
+
+    
+    var selectLanguage = document.querySelector("input[name='langauge']:checked");
+
+    var trim = userInput.value.trim();
+
+   
+    
+    if (userInput.value === "") {
+        
+    } else {
+
+        if (names[trim] === undefined) {
+
+            if (!selectLanguage) {
+                returnUser.innerHTML = trim + " !";
+                counter++;
+            } else if (selectLanguage) {
+
+                returnUser.innerHTML = trim + " !";
+
+                if (selectLanguage.value === "isixhosa") {
+                    greetUser.innerHTML = greetInIsixhosa;
+                    counter++;
+                } else if (selectLanguage.value === "sesotho") {
+                    greetUser.innerHTML = greetInSesotho;
+                    counter++;
+                } else if (selectLanguage.value === "xitsonga") {
+                    greetUser.innerHTML = greetInXitsonga;
+                    counter++;
+                }
+            }
+
+            names[userInput.value] = 1;
+        } else if (names.hasOwnProperty(trim)){
+
+        }
+
+    }
+    
+    countUsers.innerHTML = counter;
+    userInput.value = "";
+    localStorage['usersGreeted'] = counter;
+    
+}
+
+greetUserBtn.addEventListener('click', greetFunction);
+
+countUsers.innerHTML = localStorage['usersGreeted'];
+
+if (localStorage['usersGreeted']) {
+    counter = Number(localStorage['usersGreeted']);
+}
+
+
+function clearUserFunction() {
+    counter = 0;
+    localStorage['usersGreeted'] = counter;
+    returnUser.innerHTML = " . . .";
+    greetUser.innerHTML = "Hello, ";
+    countUsers.innerHTML = counter;
+
+    names = {};
+    
+}
+
+clearUserBtn.addEventListener('click', clearUserFunction);
+
