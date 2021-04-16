b767618e7a939ee2fa8e3e6cd2f1a1a37f4df244
Okuhle_Mhambi <bukhosimhambi@gmail.com>
2021-04-16 16:53:28 +0200

    commit all

--- greet-tests/greet-factory.js
+++ greet-tests/greet-factory.js
@@ -1,23 +1,95 @@
 function greetFactoryFunction(){
 
+    var alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
     var theName = "";
     var theNameObject = {};
     var theCounter = 0;
 
-    function setName(){
+    var theXhosa = "Mholo, ";
+    var theTsonga = "Ahee, ";
+    var theSotho = "Dumelang, ";
+
+    function setIsixhosa(xhosa){
+        theXhosa = xhosa;
+    }
+
+    function setSesotho(sotho){
+        theSotho = sotho;
+    }
+    function setXitsonga(tsonga){
+        theTsonga = tsonga;
+    }
+
+    function setName(name){
+        theName = name;
+    }
+
+    function setLanguage(language){
+        if(!language){
+            return "Please select your language!";
+        }else if (language === ){
+                greetIsiXhosa;
+        }if(language === greetSesotho){}
+
 
     }
+
     function setCounter(){
-
+        
     }
+   
+
+    function greetIsiXhosa(){
+        return theXhosa + theName;
+    }
+
+    function greetSesotho(){
+        return theSotho + theName;
+    }
+
+    function greetXitsonga(){
+        return theTsonga + theName;
+    }
+
     function getName(){
+        return theName;
+    }
 
+    function setErrorMessages(userInput){
+        if (userInput === ""){
+            return "Please enter yor name!";
+        }if (userInput === !alphabets){
+            return "Please enter a valid name!";
+        }if (useInput === th){
+            return "This name already exists!"
+        }
     }
-    function getTotalNumber(){
-        
+
+    function getCounter(){
+        return theCounter;
     }
+
+
+    
     return {
+        setIsixhosa,
+        setSesotho,
+        setXitsonga,
+
+        setCounter,
+        setName,
         
+        setErrorMessages,
+
+        greetIsiXhosa,
+        greetSesotho,
+        greetXitsonga,
+
+        getCounter,
+        getName
+
+
     }
 
 }
--- greet-tests/greet.js
+++ greet-tests/greet.js
@@ -1,21 +1,34 @@
 var userInput = document.querySelector(".enterName");
 var greetUser = document.querySelector(".greet");
 var returnUser = document.querySelector(".setOutput");
+var noValue = document.querySelector(".error");
 
 var greetUserBtn = document.querySelector(".getName");
-var clearUserBtn = document.querySelector(".clearCount");
+var clearUserBtn = document.querySelector(".clearCount")
 
 var countUsers = document.querySelector(".counter");
+
+var nameList = document.querySelector(".names")
+var nameListDiv = document.querySelector(".namesGreeted")
+
 var counter = 0;
+
 var names = {};
+var namesGreeted = {};
+
+var xhosaUsers = 0;
+var sothoUsers = 0;
+var tsonga = 0;
 
 var greetInIsixhosa = "Mholo, ";
 var greetInSesotho = "Dumelang, ";
 var greetInXitsonga = "Ahee, ";
 
+nameListDiv.innerHTML = "People Greeted";
+
 function greetFunction() {
 
-    
+    var alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var selectLanguage = document.querySelector("input[name='langauge']:checked");
 
     var name = userInput.value.trim();
@@ -23,62 +36,83 @@ function greetFunction() {
    
     
     if (userInput.value === "") {
-        
+        noValue.innerHTML = "Please enter your name!"
     } else {
 
         if (names[name] === undefined) {
 
+            
             if (!selectLanguage) {
-                returnUser.innerHTML = name;
-                counter++;
-            } else if (selectLanguage) {
+                noValue.innerHTML = "Please select your language!";
+                // counter++;
+            } else if (!selectLanguage && userInput.value === ""){
+                noValue.innerHTML = "Please select your language enter your name!"
+            }else if (selectLanguage) {
 
                 returnUser.innerHTML = name;
-
+                nameList.innerHTML = name;
                 if (selectLanguage.value === "isixhosa") {
                     greetUser.innerHTML = greetInIsixhosa;
+                    //namesGreeted.push(names);
+                    namesGreeted++;
                     counter++;
                 } else if (selectLanguage.value === "sesotho") {
                     greetUser.innerHTML = greetInSesotho;
+                    namesGreeted++;
                     counter++;
-                } else if (selectLanguage.value === "xitsonga") {
+                } if (selectLanguage.value === "xitsonga") {
                     greetUser.innerHTML = greetInXitsonga;
+                    namesGreeted++;
                     counter++;
-                }
+                } 
             }
 
             names[userInput.value] = 1;
+            namesGreeted[userInput.value] = names;
         } else if (names.hasOwnProperty(name)){
+            noValue.innerHTML = "This name already exists!";
 
+        }if (name != alphabets){
+            noValue.innerHTML = "Please enter a valid name!";
+        
         }
 
     }
     
     countUsers.innerHTML = counter;
     userInput.value = "";
+    
     localStorage['usersGreeted'] = counter;
+    localStorage.setItem("userNames", JSON.stringify(names));
     
 }
-
 greetUserBtn.addEventListener('click', greetFunction);
 
-countUsers.innerHTML = localStorage['usersGreeted'];
+
 
 if (localStorage['usersGreeted']) {
     counter = Number(localStorage['usersGreeted']);
 }
+countUsers.innerHTML = localStorage['usersGreeted'];
 
 
-function clearUserFunction() {
-    counter = 0;
-    localStorage['usersGreeted'] = counter;
-    returnUser.innerHTML = " . . .";
-    greetUser.innerHTML = "Hello, ";
-    countUsers.innerHTML = counter;
+if (localStorage['userNames']){
 
-    names = {};
-    
+    localStorage.getItem('userNames', JSON.stringify(names));
+
 }
 
-clearUserBtn.addEventListener('click', clearUserFunction);
+nameList.innerHTML = "";
+
+
+function clearNumberFunction() {
+    counter = 0;
+    localStorage['usersGreeted'] = counter;
+    returnUser.innerHTML = "";
+    greetUser.innerHTML = "";
+    countUsers.innerHTML = counter;
+
+    names = {};  
+}
+clearUserBtn.addEventListener('click', clearNumberFunction);
 
