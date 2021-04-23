0349e6c00b60cd6c09a6f687c631d1b6d8f741e4
Okuhle_Mhambi <bukhosimhambi@gmail.com>
2021-04-23 12:40:14 +0200

    now

--- greet.js
+++ greet.js
@@ -24,25 +24,30 @@ var greetInIsixhosa = "Mholo, ";
 var greetInSesotho = "Dumelang, ";
 var greetInXitsonga = "Ahee, ";
 
-// nameListDiv.innerHTML = "People Greeted";
+nameListDiv.innerHTML = "People Greeted";
 
 function greetFunction() {
 
-    var alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    var alphabets = /[a-zA-Z]\d+/;
+    // var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
     var selectLanguage = document.querySelector("input[name='langauge']:checked");
 
     var name = userInput.value.trim();
-
-   
     
     if (userInput.value === "") {
         
         noValue.innerHTML = "Please enter your name!";
         setTimeout(function(){
             noValue.innerHTML = "";
-        }, 2000);
+        }, 3000);
       
+    }else if(userInput.value === "" && !selectLanguage){
+        noValue.innerHTML = "Please enter your name and select your language!";
+        setTimeout(function(){
+            noValue.innerHTML = "";
+        }, 3000);
+    }if (!alphabets){
+        return "Invalid name"
     } else {
 
         if (names[name] === undefined) {
@@ -52,13 +57,19 @@ function greetFunction() {
                 noValue.innerHTML = "Please select your language!";
                 setTimeout(function(){
                     noValue.innerHTML = "";
-                }, 2000);
+                }, 3000);
+                if (userInput.value === "" ) {
+        
+                    noValue.innerHTML = "Please enter your name!";
+                    setTimeout(function(){
+                        noValue.innerHTML = "";
+                    }, 3000);}
               
-            }if (!alphabets){
-                noValue.innerHTML = "Please enter a valid name!";
+            }if (!selectLanguage && userInput.value === ""){
+                noValue.innerHTML = "Please enter your name and select your language!";
                 setTimeout(function(){
                     noValue.innerHTML = "";
-                }, 2000);
+                }, 3000);
               
             }
                 else if (selectLanguage) {
@@ -78,31 +89,35 @@ function greetFunction() {
             }
             
             namesGreeted[userInput.value] = names;
-            // nameList.innerHTML = name;
+            // nameList.innerHTML = names;
+            // console.log(names);
+            
             names[userInput.value] = 1;
         } else if (names.hasOwnProperty(name)){
              noValue.innerHTML = "This name already exists!";
              setTimeout(function(){
                 noValue.innerHTML = "";
-            }, 2000);    
+            }, 3000);    
         }
 
     }
-    
     countUsers.innerHTML = counter;
     userInput.value = "";
     
     localStorage['usersGreeted'] = counter;
     localStorage.setItem("userNames", JSON.stringify(names));
-    
+
+    deselectRadioBtns();
+    getAndDisplayNames();
+    validateInput();
 }
+
 greetUserBtn.addEventListener('click', greetFunction);
-
-
 
 if (localStorage['usersGreeted']) {
     counter = Number(localStorage['usersGreeted']);
 }
+
 countUsers.innerHTML = localStorage['usersGreeted'];
 
 
@@ -110,10 +125,9 @@ if (localStorage['userNames']){
 
     localStorage.getItem('userNames', JSON.stringify(names));
     namesGreeted = JSON.parse(localStorage['userNames']);
-
 }
 
-nameList.innerHTML = "";
+// nameList.innerHTML = "";
 
 function clearNumberFunction() {
     counter = 0;
@@ -124,7 +138,57 @@ function clearNumberFunction() {
 
     names = {};  
 }
+
 clearUserBtn.addEventListener('click', clearNumberFunction);
 
 
 
+// Displaying the names from the local storage.
+function getAndDisplayNames() {
+    // FIRST, WE CREATE AN UNORDERED LIST (ul) THAT WILL HOLD ALL THE NAMES
+    var peopleGreetedList = document.createElement('ul');
+    peopleGreetedList.className = 'names';
+    // HERE, WE APPEND THE peopleGreetedList TO THE nameListDiv
+    nameListDiv.append(peopleGreetedList);
+
+    //  HERE, WE GET ALL THE NAMES IN LOCAL STORAGE
+    var localStorageNames = JSON.parse(localStorage.getItem('userNames'));
+    
+    // Object.keys() FUNCTION MAKES AN ARRAY OF THE "keys" IN THE OBJECT, SO THAT ITS EASIER TO LOOP THROUGH
+    const namesGreeted = Object.keys(localStorageNames)    
+
+    // NOW, WE LOOP THROUGH THE LIST OF NAMES AND DISPLAY THEM TO THE USER
+    namesGreeted.forEach(name => {
+        peopleGreetedList.innerHTML += `<li>${name}</li>`;    
+    });
+}
+
+// getAndDisplayNames();
+
+// Using the regex thingy to make sure that the code does not take numbers and characters and also does not mix them together. 
+
+// Also I need an error that will return"Please enter your name and select your language.
+
+// And after the name has been greeted. The language needs to be deselected.
+// THIS FUNCTION WILL DESELECT ANY RADIO BUTTON THAT IS CHECKED
+function deselectRadioBtns() {
+    // GET ALL THE RADIO INPUTS 
+    var radios = document.querySelectorAll("[type='radio']");
+
+    // LOOP THROUGH THE ARRAY
+    radios.forEach(radio => {
+        if(radio.checked == true) radio.checked = false;
+    });
+}
+
+function validateInput() {
+    var name = userInput.value;
+    var alphabets = /[a-zA-Z]\d+/;
+    for (var i = 0; i < name.length; i++){
+        if (name !== alphabets){
+            return "PLease enter a valid name!";
+        }else if (name.includes(alphabets)){
+            return name;
+        }
+    }
+}
